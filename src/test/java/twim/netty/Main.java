b053dd1b4bb564830c5580bbf1fb96e8d884dd29
hooyn <hooyn_industry@naver.com>
2022-08-25 17:38:27 +0900

    [22.8.25] Baekjoon 등록

--- src/test/java/twim/netty/Main.java
+++ src/test/java/twim/netty/Main.java
@@ -1,15 +1,8 @@
 package twim.netty;
 
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) {
-        String input = "0036A9901FFFFFFFFFFFFFFFA{0:0}0040A9914FFFFFFFFFFFFFFFB{0:0,1:1}0044A9914FFFFFFFFFFFFFFFC{0:0,1:1,2:2}";
-        StringTokenizer st = new StringTokenizer(input, "}");
-
-        int count = st.countTokens();
-        for (int i = 0; i < count; i++) {
-            System.out.println(st.nextToken());
-        }
+        System.out.println(420);
+        System.out.println("hooyn_industry");
     }
 }
